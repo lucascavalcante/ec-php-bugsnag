29b301bb4f1e7c5e4ee3051606134c10a3408014
lucascavalcante <lucascavalcante@gmail.com>
2021-09-27 15:38:52 -0300

    Removing notification flow

--- ms-transaction/src/app/Services/TransactionService.php
+++ ms-transaction/src/app/Services/TransactionService.php
@@ -6,7 +6,6 @@ use App\Repositories\Interfaces\TransactionRepositoryInterface;
 use App\Services\Interfaces\TransactionServiceInterface;
 use App\Enums\Messages;
 use App\Repositories\Interfaces\UserRepositoryInterface;
-use App\Models\Transaction;
 
 class TransactionService implements TransactionServiceInterface
 {
@@ -20,20 +19,6 @@ class TransactionService implements TransactionServiceInterface
     {
         $transaction = $this->repository->persist($data);
 
-        $this->sendNotification($transaction);
-
         return Messages::TRANSACTION_SUCCESSFULLY;
-    }
-
-    private function sendNotification(Transaction $transaction): void
-    {
-        $user = $this->userRepository->findById($transaction['payee']);
-
-        dd([
-            'transaction' => $transaction['id'],
-            'name' => $user['name'],
-            'email' => $user['email'],
-            'value' => $transaction['value']
-        ]);
     }
 }
